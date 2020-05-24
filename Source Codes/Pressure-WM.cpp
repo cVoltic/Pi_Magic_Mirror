5d94f5a436b81afc683008b073db8ce0553a0592
Ken Trinh <ktrinh@KT-Workbook.physics>
2020-05-24 11:37:50 -0400

    New Project Repo

---
+++ Source Codes/Pressure-WM.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<math>
+using namespace std;
+/*************************************************************************************
+Ken Trinh & Co. 
+MagMirr Working Specs:
+Control sensors to take in mass/weight input (Conversion might be neccessary) and stored input in a designated server based storage. Working version includes all neccessities to run a sensor module and will use a designated storage system instead of a server for easy access.
+ 
+Date of Creation: 					12-13-2016
+Date of Modification:			12-13-2016
+
+Copyright and Property of Ken Trinh
+*************************************************************************************/
+int main(){
+	//delcared variables for storing values
+	float Pressure = 0;
+	//read modules command (Might changes depending on the sensor that we are using)
+	//Pressure -> Mass Conversion
+	//Pressure -> Weight Conversion
+	//Earth Gravity = 9.807 m/s^2
+	//Locate Designated Drive
+	//Store Data as an array
+
+	return 0; 
+}
